9f9b7311141ba8e95f302ab8ff069ad8db70e100
Эдгар Неверовский <edgarneverovskij@gmail.com>
2022-01-30 23:56:28 +0200

    Добавьте файлы проекта.

---
+++ Dictionary/Dictionary.py
@@ -0,0 +1,38 @@
+﻿from Module import*
+from random import*
+sonastik = {}
+file = open("C_C.txt","r")
+for line in file:
+    k,v = line.strip().split("-")
+    sonastik[k.strip()] = v.strip()
+
+vastus = True
+while vastus:
+    print("""
+    1. Kõik riigid ja pealinnad
+    2. Polnoreff
+    3. Uus riik või pealinn
+    4. Viga parandamine
+    5. Test
+    6. Rääkimine
+    7. Välja minna""")
+    vastus = input(" >>> ")
+    if vastus == "1":
+        print(sonastik)
+    elif vastus == "2":
+        print()
+    elif vastus == "3":
+        print()
+    elif vastus == "4":
+        print()
+    elif vastus == "5":
+        print()
+    elif vastus == "6":
+        print()
+        #text=input("->")
+        #keel=input("->")
+        #heli(text,keel)
+    elif vastus == "7":
+        vastus = False
+    else:
+        print("Vale andmetüüp!")
